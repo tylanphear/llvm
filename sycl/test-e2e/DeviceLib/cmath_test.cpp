ee86903d8f7e2f6b9b07536a20f392dae5c471fb
Chenyang Liu <chenyang.liu@intel.com>
2023-08-01 14:48:29 -0700

    mark 2 sycl tests XFAIL

--- sycl/test-e2e/DeviceLib/cmath_test.cpp
+++ sycl/test-e2e/DeviceLib/cmath_test.cpp
@@ -7,6 +7,9 @@
 // RUN: %{build} -fno-builtin -fsycl-device-lib-jit-link %{mathflags} -o %t.out
 // RUN: %if !gpu %{ %{run} %t.out %}
 
+// TODO: fix unexpected intrinsic error
+// XFAIL: *
+
 #include "math_utils.hpp"
 #include <cmath>
 #include <cstdint>
